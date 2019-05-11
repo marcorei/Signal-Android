6c14e010da294e51c32e768b61e77fc1d7469356
Markus Riegel <riegel.markus@googlemail.com>
2019-05-11 16:34:38 +0200

    Fix missing import statement

--- src/org/thoughtcrime/securesms/ConversationListFragment.java
+++ src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -27,6 +27,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
